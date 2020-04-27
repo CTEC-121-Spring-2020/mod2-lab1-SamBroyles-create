fa7efaa4f138ec6d84c7d74ce5980b52a2b73c8c
Sam Broyles <s.broyles@students.clark.edu>
2020-04-27 12:25:06 -0700

    Added code from class demo

--- Template.py
+++ Template.py
@@ -1,8 +1,8 @@
 """
 CTEC 121
-<your name>
-<assignment/lab name>
-<assignment/lab description
+Sam Broyles
+Module 2 Lab 1
+Class Demo
 """
 
 """ IPO template
@@ -12,6 +12,44 @@ Output: return value and description
 """
 
 def main():
-    # code here
+    '''
+    A = 2
+    print("\n", A, "\n")
+    a = 22
+    print(a)
+    print()
 
-main()    
+    AAA = 2
+    print(AAA)
+    spam = 222
+    print()
+    print(spam)
+    #print(Spam)
+    print()
+    
+    # expression
+    print(2)
+    print(2 + ((3 - 1) / 2))
+    print(5/3)
+    print(5//3)
+    print(5%3)
+    
+    print()
+    print("The answer is:", 3 + 4, sep=" ", end="\n")
+    print()
+
+    for i in range(5):
+        print(i, end="\t")
+    print()
+    '''
+
+    # exercise
+    classCode = "CTEC 121"
+    classCount = 18
+    className = "Intro to Programming and Problem Solving"
+    print()
+    print("Welcome to ", classCode, ". The student count is ", classCount," and the name of the class is ", className, ".",  sep="")
+    print()
+
+
+main()
